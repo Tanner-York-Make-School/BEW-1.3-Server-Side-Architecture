81cd9a4aa6a7993c980128bb80ceb9c6a9c0d66c
TannerYork <tannerwyork@gmail.com>
2021-04-07 17:27:50 -0500

    Add completed promises-challenges assignment

---
+++ Submissions/Assignments/promises-challenges/challenges/challenge2.js
@@ -0,0 +1,110 @@
+/*
+ *******************************************************************************
+ * INSTRUCTIONS:
+ * Follow the steps below and answer the discusssion questions that follow.
+ * 
+ * 1. Read over the code that follows. What will be printed to the console when
+ *    it runs? Run the code using `node challenge2.js` and verify that your
+ *    expectation was correct.
+ * 
+ *    Hello there, Ducky
+ *    Null or ' '
+ * 
+ *    I was wrong, I wasn't sure of the return statement would wait for the new
+ *    promise to complete or if it would just returns the promise right away.
+ * 
+ * 2. What happens if greet() fails? Make it fail by changing 'name' to a number
+ *    instead of a string. What happens? Does uppercaser() still run?
+ * 
+ *    When greet() fails, the catch statement is called to print 'Received an error!'
+ *    and the reject from gretting() 'Name must be a string!' and upercaser() is 
+ *    not called.
+ * 
+ * 3. What happens if greet() succeeds and uppercaser() fails? Modify your code
+ *    to achieve this result by changing the values of 'name' and 'my_str' and
+ *    run the code again.
+ * 
+ *    When uppercase() fails, the output of greet() is printed properly and then 
+ *    the catch statments gets the reject from uppercase() and prints 
+ *    'Received an error!' and 'Argument to uppercaser must be string'.
+ * 
+ * 4. Write a method that takes a string as input and returns the input string
+ *    with a space added between each character. E.g. 'foo' -> 'f o o'
+ * 
+ *    Name this method spacer(str). It should run asynchronously, so use a 
+ *    setTimeout() and return a Promise.
+ * 
+ *    Last, call spacer() after you call greeter() and uppercaser().
+ * 
+ *    Make sure you only have one catch() block. If you have more than one,
+ *    refactor your code so that you only have one. 
+ * 
+ *******************************************************************************
+ */
+
+ /**
+  * Asynchronously returns a greeting for a specified name.
+  * @param name The name of the person to greet.
+  */
+function greet(name) {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        if (typeof name === 'string') { 
+          resolve('Hello there, ' + name);
+        } else {
+          reject('Name must be a string!');
+        }
+      }, 1000);
+    });
+}
+
+/**
+ * Returns the uppercased version of a string.
+ * @param {*} str The string to uppercase.
+ */
+function uppercaser(str) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+        if (typeof str === 'string') {
+            resolve(str.toUpperCase());
+        } else {
+            reject('Argument to uppercaser must be string');
+        }
+        }, 1500);
+    });
+}
+
+/**
+ * Returns the 's p a c e d' version of a string.
+ * @param {*} str 
+ */
+function spacer(str) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof str === 'string') {
+        resolve(str.split('').join(' '));
+      } else {
+        reject('Argument to spacer must be string');
+      }
+    }, 1000);
+  });
+}
+
+name = 'Ducky'
+my_str = 'Welcome to Make School!!!'
+
+greet(name)
+    .then((greetResult) => {
+        console.log(greetResult)
+        return uppercaser(my_str);
+    })
+    .then((uppercaserResult) => {
+        console.log(uppercaserResult)
+        return spacer(name)
+    })
+    .then((spacerResult) => {
+        console.log(spacerResult)
+    }).catch((err) => {
+        console.log('Received an error!')
+        console.log(err);
+    });
